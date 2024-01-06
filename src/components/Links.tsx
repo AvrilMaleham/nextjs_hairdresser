44b90b48cdf2df4528cf51482e04ac1901e4a13b
Avril Maleham <avrilmaleham@outlook.com>
2024-01-07 11:35:18 +1300

    style: add active tab styling

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,29 +1,43 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Links = () => {
-
   const link = [
-    { 
-        title: "Contact ", 
-        path: "/contact" 
+    {
+      title: "Contact ",
+      path: "/contact",
     },
-    { 
-        title: "Portfolio", 
-        path: "/portfolio" 
+    {
+      title: "Portfolio",
+      path: "/portfolio",
     },
-    { 
-        title: "Prices", 
-        path: "/prices" 
+    {
+      title: "Prices",
+      path: "/prices",
     },
   ];
 
+  const pathName = usePathname();
+
   return (
-  <div  className="flex items-center gap-5">
-    {link.map((link => (
-        <Link href={link.path} key={link.title}>{link.title}</Link>
-    )))}
-   </div>
-    );
+    <div className="flex items-center gap-5">
+      {link.map((link) => (
+        <Link
+          href={link.path}
+          key={link.title}
+          className={`${
+            pathName === link.path
+              ? "bg-text text-bg rounded-full p-2" // Apply active styles
+              : "" // Apply normal styles
+          }`}
+        >
+          {link.title}
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Links;
