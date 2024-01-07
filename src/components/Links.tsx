a251121a40bf9f0dfc52bd18f84afc04a765f0a9
Avril Maleham <avrilmaleham@outlook.com>
2024-01-07 13:36:46 +1300

    style: add menu button and links for responsive nav

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -2,40 +2,72 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+
+interface LinkItem {
+  title: string;
+  path: string;
+}
+
+const link: LinkItem[] = [
+  {
+    title: "Contact ",
+    path: "/contact",
+  },
+  {
+    title: "Portfolio",
+    path: "/portfolio",
+  },
+  {
+    title: "Prices",
+    path: "/prices",
+  },
+];
 
 const Links = () => {
-  const link = [
-    {
-      title: "Contact ",
-      path: "/contact",
-    },
-    {
-      title: "Portfolio",
-      path: "/portfolio",
-    },
-    {
-      title: "Prices",
-      path: "/prices",
-    },
-  ];
+  const [isOpen, setIsOpen] = useState(false);
 
   const pathName = usePathname();
 
   return (
-    <div className="flex items-center gap-5">
-      {link.map((link) => (
-        <Link
-          href={link.path}
-          key={link.title}
-          className={`${
-            pathName === link.path
-              ? "bg-text text-bg rounded-full p-2" // Apply active styles
-              : "" // Apply normal styles
-          }`}
-        >
-          {link.title}
-        </Link>
-      ))}
+    <div>
+      <div className="hidden md:flex items-center gap-5">
+        {link.map((link) => (
+          <Link
+            href={link.path}
+            key={link.title}
+            className={`${
+              pathName === link.path
+                ? "bg-text text-bg rounded-full p-2" // Apply active styles
+                : "" // Apply normal styles
+            }`}
+          >
+            {link.title}
+          </Link>
+        ))}
+      </div>
+
+      <button className=" md:hidden text-xl font-bold" onClick={() => setIsOpen((prev) => !prev)}>
+        Menu
+      </button>
+
+      {isOpen && (
+        <div className="flex flex-col items-center gap-5">
+          {link.map((linkItem) => (
+            <Link
+              href={linkItem.path}
+              key={linkItem.title}
+              className={`${
+                pathName === linkItem.path
+                  ? "bg-text text-bg rounded-full p-2" // Apply active styles
+                  : "" // Apply normal styles
+              }`}
+            >
+              {linkItem.title}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
