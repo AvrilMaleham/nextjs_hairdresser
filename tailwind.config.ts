012bb9adb6cda5322221fbe2ccaad1688e6a32c9
Avril Maleham <avrilmaleham@outlook.com>
2024-01-12 21:37:14 +1300

    style: edit global css file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,11 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        bg: '#0d0c22',
-        bgSoft: '#2d2b42',
-        text: 'white',
-        textSoft: '#e5e5e5',
-        btn: '#3673fd',
+        // bg: '#0d0c22',
+        // bgSoft: '#2d2b42',
+        // text: 'white',
+        // textSoft: '#e5e5e5',
+        // btn: '#3673fd',
       },
     },
   },
