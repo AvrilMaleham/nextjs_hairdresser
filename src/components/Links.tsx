6ab23faedc612287397131ca32238bd360fb273e
Avril Maleham <avrilmaleham@outlook.com>
2024-01-15 16:03:38 +1300

    style: transition working

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
+import clsx from "clsx";
 
 interface LinkItem {
   title: string;
@@ -56,33 +57,38 @@ const Links = () => {
       </button>
 
       {/* Sidebar menu */}
-      {isOpen && (
-        // Blur background
-        <div className="fixed h-full w-screen bg-backgroundCol/50 backdrop-blur-sm top-0 right-0 md:hidden">
-          <div className="flex flex-col items-center justify-center gap-10 absolute top-0 right-0 w-1/2 h-full bg-backgroundCol md:hidden">
-            {link.map((linkItem) => (
-              <Link
-                href={linkItem.path}
-                key={linkItem.title}
-                className={`${
-                  pathName === linkItem.path
-                    ? "bg-textCol text-backgroundCol rounded-full p-2" // Apply active styles
-                    : "" // Apply normal styles
-                }`}
-              >
-                {linkItem.title}
-              </Link>
-            ))}
-          </div>
 
-          <button
-            className="text-xl font-bold z-50 absolute m-8 right-5 top-5 md:hidden"
-            onClick={() => setIsOpen(false)}
-          >
-            X
-          </button>
+      {/* Blur background */}
+      {/* <div className={clsx("fixed h-full w-screen bg-backgroundCol/50 backdrop-blur-sm top-0 right-0 translate-x-[1000px] transition-all md:hidden",
+        isOpen && "-translate-x-0")}> */}
+      <div
+        className={`${
+          isOpen ? "translate-x-0" : "translate-x-[1000px]"
+        } fixed h-full w-screen bg-backgroundCol/50 backdrop-blur-sm top-0 right-0 transition-all md:hidden`}
+      >
+        <div className="flex flex-col items-center justify-center gap-10 absolute top-0 right-0 w-1/2 h-full bg-backgroundCol md:hidden">
+          {link.map((linkItem) => (
+            <Link
+              href={linkItem.path}
+              key={linkItem.title}
+              className={`${
+                pathName === linkItem.path
+                  ? "bg-textCol text-backgroundCol rounded-full p-2" // Apply active styles
+                  : "" // Apply normal styles
+              }`}
+            >
+              {linkItem.title}
+            </Link>
+          ))}
         </div>
-      )}
+
+        <button
+          className="text-xl font-bold z-50 absolute m-8 right-5 top-5 md:hidden"
+          onClick={() => setIsOpen(false)}
+        >
+          X
+        </button>
+      </div>
     </div>
   );
 };
